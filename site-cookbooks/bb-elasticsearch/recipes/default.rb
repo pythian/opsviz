adf1f0122aec69c5bfe77172eb3419fff85546d7
Taylor Ludwig <taylorludwig@gmail.com>
2014-10-14 12:30:15 -0700

    correct attribute for http-basic plugin url

--- site-cookbooks/bb-elasticsearch/recipes/default.rb
+++ site-cookbooks/bb-elasticsearch/recipes/default.rb
@@ -13,5 +13,5 @@ include_recipe 'java'
 include_recipe 'elasticsearch'
 
 if node[:elasticsearch][:http_auth_plugin]
-  install_plugin 'http-basic', url: node[:elasticsearch][:http_auth]
+  install_plugin 'http-basic', url: node[:elasticsearch][:http_auth_plugin]
 end
