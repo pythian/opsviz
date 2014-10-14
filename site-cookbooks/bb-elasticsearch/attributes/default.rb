3f146b2a5d1157ead9a5f4869b796eca4df8a984
Taylor Ludwig <taylorludwig@gmail.com>
2014-10-14 13:48:51 -0700

    override custom config options to pass in http auth pass

--- site-cookbooks/bb-elasticsearch/attributes/default.rb
+++ site-cookbooks/bb-elasticsearch/attributes/default.rb
@@ -2,9 +2,10 @@ default[:elasticsearch][:http_auth] = false
 default[:elasticsearch][:http_auth_plugin] = 'https://github.com/Asquera/elasticsearch-http-basic/releases/download/1.3.2/elasticsearch-http-basic-1.3.2.jar'
 
 if node[:elasticsearch][:http_auth]
-  default[:elasticsearch][:custom_config][:basic_auth] = true
-  default[:elasticsearch][:custom_config][:user] = node[:elasticsearch][:basic_auth][:user]
-  default[:elasticsearch][:custom_config][:password] = node[:elasticsearch][:basic_auth][:password]
+  normal[:elasticsearch][:custom_config] = {}
+  normal[:elasticsearch][:custom_config][:basic_auth] = true
+  normal[:elasticsearch][:custom_config][:user] = node[:elasticsearch][:basic_auth][:user]
+  normal[:elasticsearch][:custom_config][:password] = node[:elasticsearch][:basic_auth][:password]
 end
 
 normal[:java][:install_flavor] = 'openjdk'
