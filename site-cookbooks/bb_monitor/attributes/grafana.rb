26cc185c1fd348a06f4bf916346fa3bf5e431dd9
Taylor Ludwig <taylorludwig@gmail.com>
2014-10-23 14:45:18 -0700

    initial grafana install

---
+++ site-cookbooks/bb_monitor/attributes/grafana.rb
@@ -0,0 +1,16 @@
+normal['grafana']['datasources'] = {
+  'graphite' => {
+    'type' => "'graphite'",
+    'url'  => 'window.location.protocol+"//"+window.location.hostname+":"+window.location.port+"/"',
+    'default' => true
+  },
+  'elasticsearch' => {
+    'type' => "'elasticsearch'",
+    'url'  => 'window.location.protocol+"//"+window.location.hostname+":"+window.location.port+"/elasticsearch"',
+    'index' => "'#{node['grafana']['grafana_index']}'",
+    'grafanaDB' => true
+  }
+}
+
+# We don't want grana to install nginx for us, we'll do that
+normal['grafana']['webserver'] = ''
