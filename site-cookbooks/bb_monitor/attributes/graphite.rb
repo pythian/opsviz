05bc78d29165ef85d3ca56af6206d51f39cfed7b
Taylor Ludwig <taylorludwig@gmail.com>
2014-10-23 13:01:32 -0700

    correct attribute for overriding graphite web port

--- site-cookbooks/bb_monitor/attributes/graphite.rb
+++ site-cookbooks/bb_monitor/attributes/graphite.rb
@@ -1,2 +1,3 @@
 # Install graphite web on non standard port so we can nginx reverse proxy to it behind auth
-normal['graphite']['uwsgi']['listen_http'] = "8081"
+normal['graphite']['uwsgi']['listen_http'] = true
+normal['graphite']['listen_port'] = "8081"
