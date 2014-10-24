905e38605c07faedff0490ee200106a09a67532d
Taylor Ludwig <taylorludwig@gmail.com>
2014-10-24 11:45:18 -0700

    remove spaces from stack name for sensu client name

--- site-cookbooks/bb_monitor/recipes/sensu_client.rb
+++ site-cookbooks/bb_monitor/recipes/sensu_client.rb
@@ -1,6 +1,6 @@
 include_recipe "sensu::default"
 
-sensu_client "#{node[:opsworks][:instance][:hostname]}.#{node[:opsworks][:instance][:layers][0]}.#{node[:opsworks][:stack][:name].downcase}" do
+sensu_client "#{node[:opsworks][:instance][:hostname]}.#{node[:opsworks][:instance][:layers][0]}.#{node[:opsworks][:stack][:name].downcase.gsub(' ','_')}" do
   address "#{node['opsworks']['instance']['private_ip']}"
   subscriptions node[:bb_monitor][:sensu][:subscriptions]
   additional ({
--- site-cookbooks/bb_monitor/recipes/sensu_client_remove.rb
+++ site-cookbooks/bb_monitor/recipes/sensu_client_remove.rb
@@ -1,4 +1,4 @@
 execute "remove sensu client" do
-	command "curl -X DELETE #{node[:sensu][:api][:host]}:#{node[:sensu][:api][:port]}/client/#{node[:opsworks][:instance][:hostname]}.#{node[:opsworks][:instance][:layers][0]}.#{node[:opsworks][:stack][:name].downcase}"
+	command "curl -X DELETE #{node[:sensu][:api][:host]}:#{node[:sensu][:api][:port]}/client/#{node[:opsworks][:instance][:hostname]}.#{node[:opsworks][:instance][:layers][0]}.#{node[:opsworks][:stack][:name].downcase.gsub(' ','_')}"
 	action :run
 end
