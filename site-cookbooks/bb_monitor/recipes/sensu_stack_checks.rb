47377b2835fba707ad4b45b89607f53d37ee9d5f
Taylor Ludwig <taylorludwig@gmail.com>
2014-10-27 14:25:18 -0700

    correct subscriber for layer name

--- site-cookbooks/bb_monitor/recipes/sensu_stack_checks.rb
+++ site-cookbooks/bb_monitor/recipes/sensu_stack_checks.rb
@@ -15,14 +15,14 @@ node[:opsworks][:layers].each do |key, value|
       type "metric"
       command "elb-metrics.rb -n #{elb[:name]} --scheme stats.#{node[:opsworks][:stack][:name].downcase.gsub(' ','_')}.#{key}.elb"
       handlers node[:bb_monitor][:sensu][:default_metric_handlers]
-      subscribers ["sensu"]
+      subscribers ["dashboard"]
       interval 150
     end
 
     sensu_check "check-sensu-elb-#{elb[:name]}" do
       command "check-elb-health.rb -n #{elb[:name]} -v"
       handlers node[:bb_monitor][:sensu][:default_check_handlers]
-      subscribers ["sensu"]
+      subscribers ["dashboard"]
       interval 150
       additional(:occurrences => 2)
     end
