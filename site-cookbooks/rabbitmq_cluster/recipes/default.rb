852e8e1b26017064168f4dd8a52570b10b8374dc
Alex Lovell-Troy <alex@lovelltroy.org>
2014-10-27 21:00:36 -0600

    Adding sensu user and management console

--- site-cookbooks/rabbitmq_cluster/recipes/default.rb
+++ site-cookbooks/rabbitmq_cluster/recipes/default.rb
@@ -11,7 +11,8 @@ end
 rabbit_nodes = instances.map{ |name, attrs| "rabbit@#{name}" }
 node.set['rabbitmq']['cluster_disk_nodes'] = rabbit_nodes
 
-include_recipe 'rabbitmq'
+#include_recipe 'rabbitmq'
+include_recipe 'rabbitmq::mgmt_console'
 
 execute "chown -R rabbitmq:rabbitmq /var/lib/rabbitmq"
 
@@ -35,4 +36,14 @@ rabbitmq_user node['rabbitmq_cluster']['user'] do
   vhost "/"
   permissions ".* .* .*"
   action :set_permissions
+end
+
+rabbitmq_user "sensu" do
+  password "sensu"
+  action :add
+end
+
+rabbitmq_user "sensu" do
+  tag "monitoring"
+  action :set_tags
 end
