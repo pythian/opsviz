174fa37a664fceff4d15aa5707beb7a22a747571
Taylor Ludwig <taylorludwig@gmail.com>
2014-10-28 15:50:57 -0700

    remove leftover lua dependencies

--- site-cookbooks/bb_monitor/attributes/nginx.rb
+++
@@ -1,8 +0,0 @@
-normal['nginx']['source']['modules']  = %w(
-  nginx::http_ssl_module
-  nginx::http_gzip_static_module
-  nginx::ngx_lua_module
-)
-
-normal['nginx']['lua']['version']  = '0.9.12'
-normal['nginx']['lua']['checksum'] = 'e85c1924ca4670d5708b58efcd6e77793c43f243317a9850a112964067f63150'
