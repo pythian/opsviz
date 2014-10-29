7fdb7d2f3cc4abe49f23bea013d4a47131040e50
Taylor Ludwig <taylorludwig@gmail.com>
2014-10-29 15:35:18 -0700

    remove keys from attributes, move to custom json

--- site-cookbooks/bb_monitor/attributes/doorman.rb
+++ site-cookbooks/bb_monitor/attributes/doorman.rb
@@ -1,5 +1,6 @@
 normal['nodejs']['install_method'] = 'binary'
 
-default['doorman']['session_secret'] = 'Jo9blyEg3jap0Juc2Ecs6iC1bAb4muC0jOrk1iaS0yaRc9oth3'
-default['doorman']['app_id'] = '4b7121efaa5e229eecb1'
-default['doorman']['app_secret'] = '3c56c5aeb8a6c3dc3bc5bf1bc4bf53f0720c32ed'
+# Set in custom json
+# default['doorman']['session_secret']
+# default['doorman']['app_id']
+# default['doorman']['app_secret']
