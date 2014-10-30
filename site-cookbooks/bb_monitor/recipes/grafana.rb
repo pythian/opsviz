45ce8a726323930818522ea9489599c5c5a143e3
Taylor Ludwig <taylorludwig@gmail.com>
2014-10-30 12:01:30 -0700

    add a default dashboard for grafana

--- site-cookbooks/bb_monitor/recipes/grafana.rb
+++ site-cookbooks/bb_monitor/recipes/grafana.rb
@@ -1,1 +1,8 @@
 include_recipe 'grafana'
+
+template "#{node['grafana']['install_dir']}/apps/dashboars/default.json" do
+  source 'system_stats.json.erb'
+  owner 'root'
+  group 'root'
+  mode '0664'
+end
