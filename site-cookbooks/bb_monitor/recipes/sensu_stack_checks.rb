de90f6efa1955e298bc572313aec1e13c8a4b42e
Taylor Ludwig <taylorludwig@gmail.com>
2014-10-31 13:53:16 -0700

    add region to elb checks

--- site-cookbooks/bb_monitor/recipes/sensu_stack_checks.rb
+++ site-cookbooks/bb_monitor/recipes/sensu_stack_checks.rb
@@ -13,14 +13,14 @@ node[:opsworks][:layers].each do |key, value|
   value["elb-load-balancers"].each do |elb|
     sensu_check "metric-sensu-elb-#{elb[:name]}" do
       type "metric"
-      command "elb-metrics.rb -n #{elb[:name]} --scheme stats.#{node[:opsworks][:stack][:name].downcase.gsub(' ','_')}.#{key}.elb"
+      command "elb-metrics.rb -r #{node[:aws_region]} -n #{elb[:name]} --scheme stats.#{node[:opsworks][:stack][:name].downcase.gsub(' ','_')}.#{key}.elb"
       handlers node[:bb_monitor][:sensu][:default_metric_handlers]
       subscribers ["dashboard"]
       interval 150
     end
 
     sensu_check "check-sensu-elb-#{elb[:name]}" do
-      command "check-elb-health.rb -n #{elb[:name]} -v"
+      command "check-elb-health.rb -r #{node[:aws_region]} -n #{elb[:name]} -v"
       handlers node[:bb_monitor][:sensu][:default_check_handlers]
       subscribers ["dashboard"]
       interval 150
