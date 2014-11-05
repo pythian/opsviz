973d724f578980cc22e2d2db78124a89b9efe60e
Taylor Ludwig <taylorludwig@gmail.com>
2014-11-05 10:30:51 -0800

    add stack name to route53 records

--- site-cookbooks/bb_monitor/recipes/route53.rb
+++ site-cookbooks/bb_monitor/recipes/route53.rb
@@ -1,12 +1,13 @@
-if node[:route53_zone_id] && !node[:route53_zone_id].empty?
+if node[:route53][:zone_id] && !node[:route53][:zone_id].empty?
   include_recipe "route53"
 
+  stack_name = node[:opsworks][:stack][:name].downcase
   instance_dns = node[:opsworks][:instance][:public_dns_name] || node[:opsworks][:instance][:private_dns_name]
 
   node[:opsworks][:instance][:layers].each do |layer|
     log "Creating route53 record for instance #{node[:opsworks][:instance][:hostname]}"
-    route53_record "route53 #{node[:opsworks][:instance][:hostname]}.#{layer}.#{node[:route53][:domain_name]}" do
-      name  "#{node[:opsworks][:instance][:hostname]}.#{layer}.#{node[:route53][:domain_name]}"
+    route53_record "route53 #{node[:opsworks][:instance][:hostname]}.#{layer}.#{stack_name}.#{node[:route53][:domain_name]}" do
+      name  "#{node[:opsworks][:instance][:hostname]}.#{layer}.#{stack_name}.#{node[:route53][:domain_name]}"
       value instance_dns
       type  "CNAME"
       zone_id node[:route53][:zone_id]
@@ -18,9 +19,9 @@ if node[:route53_zone_id] && !node[:route53_zone_id].empty?
     if node[:opsworks][:layers][layer][:instances].keys[0] == node[:opsworks][:instance][:hostname] && !node[:opsworks][:layers][layer]["elb-load-balancers"].empty?
       elb = node[:opsworks][:layers][layer]["elb-load-balancers"].first
 
-      log "Creating route53 record for layer #{layer}.#{node[:route53][:domain_name]}"
+      log "Creating route53 record for layer #{layer}.#{stack_name}.#{node[:route53][:domain_name]}"
       route53_record "route53 #{layer}" do
-        name  "#{layer}.#{node[:route53][:domain_name]}"
+        name  "#{layer}.#{stack_name}.#{node[:route53][:domain_name]}"
         value elb[:dns_name]
         type  "CNAME"
         zone_id node[:route53][:zone_id]
@@ -30,5 +31,6 @@ if node[:route53_zone_id] && !node[:route53_zone_id].empty?
 
     end
   end
-
+else
+  log "Skipping route53 records because zone_id is not set"
 end
