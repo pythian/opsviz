23e8d71ebe3800496bc69461cfab1427ba10e111
Taylor Ludwig <taylorludwig@gmail.com>
2014-11-05 10:20:36 -0800

    add domain name to route53 records

--- site-cookbooks/bb_monitor/recipes/route53.rb
+++ site-cookbooks/bb_monitor/recipes/route53.rb
@@ -4,11 +4,12 @@ if node[:route53_zone_id] && !node[:route53_zone_id].empty?
   instance_dns = node[:opsworks][:instance][:public_dns_name] || node[:opsworks][:instance][:private_dns_name]
 
   node[:opsworks][:instance][:layers].each do |layer|
-    route53_record "route53 #{node[:opsworks][:instance][:hostname]}.#{layer}" do
-      name  "#{node[:opsworks][:instance][:hostname]}.#{layer}"
+    log "Creating route53 record for instance #{node[:opsworks][:instance][:hostname]}"
+    route53_record "route53 #{node[:opsworks][:instance][:hostname]}.#{layer}.#{node[:route53][:domain_name]}" do
+      name  "#{node[:opsworks][:instance][:hostname]}.#{layer}.#{node[:route53][:domain_name]}"
       value instance_dns
       type  "CNAME"
-      zone_id node[:route53_zone_id]
+      zone_id node[:route53][:zone_id]
       overwrite true
       action :create
     end
@@ -17,11 +18,12 @@ if node[:route53_zone_id] && !node[:route53_zone_id].empty?
     if node[:opsworks][:layers][layer][:instances].keys[0] == node[:opsworks][:instance][:hostname] && !node[:opsworks][:layers][layer]["elb-load-balancers"].empty?
       elb = node[:opsworks][:layers][layer]["elb-load-balancers"].first
 
+      log "Creating route53 record for layer #{layer}.#{node[:route53][:domain_name]}"
       route53_record "route53 #{layer}" do
-        name  "#{layer}"
+        name  "#{layer}.#{node[:route53][:domain_name]}"
         value elb[:dns_name]
         type  "CNAME"
-        zone_id node[:route53_zone_id]
+        zone_id node[:route53][:zone_id]
         overwrite true
         action :create
       end
