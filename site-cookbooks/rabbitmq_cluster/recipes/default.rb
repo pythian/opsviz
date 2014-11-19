92bed1e1d20f6c22df457e615f80740e462d4da4
Taylor Ludwig <taylorludwig@gmail.com>
2014-11-19 11:25:31 -0800

    a rabbitmq user for statsd

--- site-cookbooks/rabbitmq_cluster/recipes/default.rb
+++ site-cookbooks/rabbitmq_cluster/recipes/default.rb
@@ -51,3 +51,20 @@ rabbitmq_user sensu_config["user"] do
   action :set_permissions
 end
 include_recipe 'rabbitmq_cluser::monitor'
+
+statsd_config = node["statsd"]["rabbitmq"]
+
+rabbitmq_vhost statsd_config["vhost"] do
+  action :add
+end
+
+rabbitmq_user statsd_config["user"] do
+  password statsd_config["password"]
+  action :add
+end
+
+rabbitmq_user statsd_config["user"] do
+  vhost statsd_config["vhost"]
+  permissions ".* .* .*"
+  action :set_permissions
+end
