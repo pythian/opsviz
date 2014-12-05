0f14392128676df239246b08f690529238e1ab5e
Taylor Ludwig <taylorludwig@gmail.com>
2014-12-05 15:40:00 -0800

    add check to make sure graphite cache isn't growing too big

--- site-cookbooks/bb_monitor/recipes/sensu_stack_checks.rb
+++ site-cookbooks/bb_monitor/recipes/sensu_stack_checks.rb
@@ -29,3 +29,11 @@ node[:opsworks][:layers].each do |key, value|
 
   end
 end
+
+sensu_check "check-graphite-cache" do
+  command "graphite.rb -h #{node[:graphite][:host]}:8081 -t carbon.agents.*.cache.size -a 100,500 -p 10minutes -g"
+  handlers node[:bb_monitor][:sensu][:default_check_handlers]
+  subscribers ["dashboard"]
+  interval 300
+  additional(:occurrences => 2)
+end
