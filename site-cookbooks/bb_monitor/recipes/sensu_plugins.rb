56ee25240d31b9d0cb614faf300bb39756746e62
Taylor Ludwig <taylorludwig@gmail.com>
2014-12-05 17:44:05 -0800

    install needed plugins

--- site-cookbooks/bb_monitor/recipes/sensu_plugins.rb
+++ site-cookbooks/bb_monitor/recipes/sensu_plugins.rb
@@ -29,6 +29,11 @@ end
   check-rabbitmq-queue.rb
   rabbitmq-cluster-health.rb
   rabbitmq-overview-metrics.rb
+  check-es-cluster-status.rb
+  check-es-file-descriptors.rb
+  check-es-heap.rb
+  es-cluster-metrics.rb
+  es-node-metrics.rb
 ].each do |plugin|
   cookbook_file ::File.join(node.sensu.directory, "plugins", plugin) do
     source "sensu_plugins/#{plugin}"
