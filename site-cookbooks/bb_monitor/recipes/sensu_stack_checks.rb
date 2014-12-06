7e310078c94b2cc93842825c837c3b92f21e812c
Taylor Ludwig <taylorludwig@gmail.com>
2014-12-05 18:34:31 -0800

    use metric handlers

--- site-cookbooks/bb_monitor/recipes/sensu_stack_checks.rb
+++ site-cookbooks/bb_monitor/recipes/sensu_stack_checks.rb
@@ -68,7 +68,7 @@ stack_name = node[:opsworks][:stack][:name].downcase.gsub(' ','_')
 sensu_check "metric-cluster-elasticsearch" do
   type "metric"
   command "es-cluster-metrics.rb -h #{node[:kibana][:elasticsearch_server]} -s stats.#{stack_name}.elasticsearch.cluster"
-  handlers node[:bb_monitor][:sensu][:default_check_handlers]
+  handlers node[:bb_monitor][:sensu][:default_metric_handlers]
   subscribers ["dashboard"]
   interval 60
 end
@@ -76,7 +76,7 @@ end
 sensu_check "metric-node-elasticsearch" do
   type "metric"
   command "es-node-metrics.rb -h #{node[:kibana][:elasticsearch_server]} -s stats.#{stack_name}.elasticsearch"
-  handlers node[:bb_monitor][:sensu][:default_check_handlers]
+  handlers node[:bb_monitor][:sensu][:default_metric_handlers]
   subscribers ["dashboard"]
   interval 60
 end
