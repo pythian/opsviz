5f59d54b804c635d9b166b61c6c9794c8ef84baf
Taylor Ludwig <taylorludwig@gmail.com>
2014-12-11 14:41:35 -0800

    cleanup the format of attributes coming in given assumptions we have on custom json (one rabbitmq output, and multiple inputs). Also make the config generic enough that we can define inputs per layer or node name

--- site-cookbooks/bb_external/recipes/logstash_agent.rb
+++ site-cookbooks/bb_external/recipes/logstash_agent.rb
@@ -1,1 +1,33 @@
+%w(host password).each do |attribute|
+  unless node[:bb_external][:logstash][:rabbitmq].has_key?(attribute)
+    raise "Missing attribute bb_external.logstash.rabbitmq.#{attribute}"
+  end
+end
+
+
+file_inputs = []
+
+groups = ['all', node.name]
+# It can run via chef solo on a single instance as well as capabilites for opsworks
+groups += node[:opsworks][:instance][:layers] if node[:bb_external][:opsworks]
+
+Chef::Log.info "Logstash file input groups"
+groups.each do |group|
+  next unless node[:bb_external][:logstash][:file_inputs].has_key?(group)
+  if node[:bb_external][:logstash][:file_inputs][group].kind_of?(Array)
+    file_inputs += node[:bb_external][:logstash][:file_inputs][group]
+  else
+    file_inputs << node[:bb_external][:logstash][:file_inputs][group]
+  end
+end
+
+Chef::Log.info "Logstash inputs #{inputs}"
+
+# Forward attributes to logstash cookbook in correct format
+node.normal[:logstash][:agent][:outputs] = [{'rabbitmq' => node[:bb_external][:logstash][:rabbitmq]}]
+node.normal[:logstash][:agent][:inputs] = file_inputs.map {|config| {"file" => config} }
+
+Chef::Log.info "Logstash config #{node[:logstash]}"
+
+
 include_recipe "logstash::default"
