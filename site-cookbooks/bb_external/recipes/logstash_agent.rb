fca5e2e54af6d687ba5802984b05fecda04ac622
Taylor Ludwig <taylorludwig@gmail.com>
2014-12-11 16:05:57 -0800

    correct template extension

--- site-cookbooks/bb_external/recipes/logstash_agent.rb
+++ site-cookbooks/bb_external/recipes/logstash_agent.rb
@@ -45,7 +45,7 @@ Chef::Log.info "Logstash config #{node[:logstash]}"
 include_recipe "logstash::default"
 
 template '/etc/init/logstash.conf' do
-  source 'logstash.init.conf'
+  source 'logstash.init.conf.erb'
   owner 'root'
   group 'root'
   mode '0755'
