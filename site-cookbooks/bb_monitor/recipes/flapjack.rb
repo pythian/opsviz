2572e054b2b98dd9fec14832249bb41178350aea
Taylor Ludwig <taylorludwig@gmail.com>
2014-12-23 12:25:56 -0800

    update repo

--- site-cookbooks/bb_monitor/recipes/flapjack.rb
+++ site-cookbooks/bb_monitor/recipes/flapjack.rb
@@ -1,7 +1,7 @@
 execute 'name' do
   command  <<-EOF
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 8406B0E3803709B6
-  echo 'deb http://packages.flapjack.io/deb precise main' | sudo tee  /etc/apt/sources.list.d/flapjack.list
+  echo 'deb http://packages.flapjack.io/deb trusty main' | sudo tee  /etc/apt/sources.list.d/flapjack.list
   sudo apt-get update
   EOF
   action :run
