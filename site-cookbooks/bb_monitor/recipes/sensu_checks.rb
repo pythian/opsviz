81407b5fa5f6d8de070a3f1889e6f4af305269c5
Alex Lovell-Troy <alex@lovelltroy.org>
2015-03-02 09:05:21 -0600

    create-stack can reuse ssl certs and sensu uses commented passwords

--- site-cookbooks/bb_monitor/recipes/sensu_checks.rb
+++ site-cookbooks/bb_monitor/recipes/sensu_checks.rb
@@ -76,7 +76,7 @@ end
 include_recipe "sensu"
 
 sensu_check "rabbitmq-messages" do
-  command "check-rabbitmq-messages.rb --user sensu_monitor --password #{node["sensu"]["rabbitmq"]["password"]} -w 600 -c 900"
+  command "check-rabbitmq-messages.rb --user sensu_monitor --password \"#{node["sensu"]["rabbitmq"]["password"]}\" -w 600 -c 900"
   handlers node[:bb_monitor][:sensu][:default_check_handlers]
   subscribers ["rabbitmq"]
   interval 60
