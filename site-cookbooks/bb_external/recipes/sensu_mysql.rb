9dbd83feb37d92c0d1cca605a07cb061d6d99ba4
Derek Downey <downey@pythian.com>
2015-03-04 20:50:43 -0500

    Adding bb_external::sensu_mysql to be run if sensu client subscribes to 'mysql'

---
+++ site-cookbooks/bb_external/recipes/sensu_mysql.rb
@@ -0,0 +1,30 @@
+# Add gem dependences for mysql plugins
+%w[
+  mysql2
+  inifile
+].each do |package|
+  gem_package package do
+    action :install
+  end
+end
+
+log "MySQL sensu plugins" do
+  level :info
+  message "MySQL plugin execution"
+end
+
+%w(password).each do |attribute|
+  unless node[:bb_external][:sensu][:mysql].has_key?(attribute)
+    raise "Missing attribute bb_external.sensu.mysql.#{attribute}"
+  end
+end
+
+# configure /opt/sensu/.my.cnf
+# TODO: If using 5.6 we can
+template "/opt/sensu/.my.cnf" do
+  source "my.cnf_sensu.erb"
+  owner "sensu"
+  group "sensu"
+  mode "0400"
+  variables( :mysql_user => node[:bb_external][:sensu][:mysql][:user], :mysql_password => node[:bb_external][:sensu][:mysql][:password] )
+end
