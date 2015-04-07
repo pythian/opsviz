0ae9e68fe9fa89b05e8f2d1981fa18a19b073fa7
Alex Lovell-Troy <alex@lovelltroy.org>
2015-04-07 16:07:08 -0400

    Adding the central dashboard and some nginx fixes

--- site-cookbooks/bb_monitor/recipes/nginx.rb
+++ site-cookbooks/bb_monitor/recipes/nginx.rb
@@ -13,6 +13,42 @@ file "/etc/nginx/sites-enabled/default" do
   action :delete
 end
 
+basedir = "/var/www/"
+
+['dashboard','dashboard/css','dashboard/fonts','dashboard/js'].each do |webdir|
+  directory basedir + webdir do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+  end
+end
+
+['dashboard/css/bootstrap-theme.css',
+'dashboard/css/bootstrap-theme.css.map',
+'dashboard/css/bootstrap-theme.min.css',
+'dashboard/css/bootstrap.css',
+'dashboard/css/bootstrap.css.map',
+'dashboard/css/bootstrap.min.css',
+'dashboard/fonts/glyphicons-halflings-regular.eot',
+'dashboard/fonts/glyphicons-halflings-regular.svg',
+'dashboard/fonts/glyphicons-halflings-regular.ttf',
+'dashboard/fonts/glyphicons-halflings-regular.woff',
+'dashboard/fonts/glyphicons-halflings-regular.woff2',
+'dashboard/index.html',
+'dashboard/js/bootstrap.js',
+'dashboard/js/bootstrap.min.js',
+'dashboard/js/npm.js'].each do |webfile|
+    cookbook_file webfile do 
+      source webfile 
+      owner 'root'
+      group 'root'
+      mode '0644'
+      path basedir + webfile
+      action :create
+    end
+end
+
 template "/etc/nginx/sites-available/dashboard" do
   source "nginx/dashboard.erb"
   owner "root"
