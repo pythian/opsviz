e7fc12c7cb374b0fc696a5fbb577c0e8ad885bed
Jordan Metzmeier <titan8990@gmail.com>
2015-05-21 08:10:14 -0500

    fix typo of redis_cluster_id variable

--- site-cookbooks/bb_monitor/libraries/redis_elasticache.rb
+++ site-cookbooks/bb_monitor/libraries/redis_elasticache.rb
@@ -7,7 +7,7 @@ module OpsVizExtensions
     region = Net::HTTP.get('169.254.169.254', '/latest/meta-data/placement/availability-zone')[0...-1]
 
     ecache = Fog::AWS::Elasticache.new({:use_iam_profile => true, :region => region})
-    resp = ecache.describe_cache_clusters(regis_cluster_id, {:show_node_info => true})
+    resp = ecache.describe_cache_clusters(redis_cluster_id, {:show_node_info => true})
     resp.body['CacheClusters'][0]['CacheNodes'][0]['Address']
   end
 end
