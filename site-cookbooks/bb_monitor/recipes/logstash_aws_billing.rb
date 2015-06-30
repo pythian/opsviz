193a65a7cbe64c5ab2d6a1f29aeefa7e4fab8632
Rock Mutchler <rock@drivedev.com>
2015-06-30 11:52:16 -0500

    moved aws-billing logstash template

--- site-cookbooks/bb_monitor/recipes/logstash_aws_billing.rb
+++ site-cookbooks/bb_monitor/recipes/logstash_aws_billing.rb
@@ -1,57 +1,13 @@
-ruby_block "Add AWS Billing Filters for Logstash" do 
-	block do
-		code[] = ""
-		code << "input {"
-		code << "  tcp {"
-		code << "    port => 5140"
-		code << "    codec => \"line\""
-		code << "  }"
-		code << "}"
-		code << ""
-		code << "filter {"
-		code << "  json {"
-		code << "    source => \"message\""
-		code << "  }"
-		code << ""
-		code << "  mutate {"
-		code << "    add_field => { \"index\" => \"logstash-%{+YYYY.MM.dd}\" }"
-		code << "  }"
-		code << ""
-		code << "  if [type] == \"aws_billing_hourly\" or [type] == \"aws_billing_monthly\" {"
-		code << "    if [type] == \"aws_billing_hourly\" {"
-		code << "      date {"
-		code << "        match => [ \"UsageStartDate\", \"ISO8601\" ]"
-		code << "      }"
-		code << "    }"
-		code << "    if [type] == \"aws_billing_monthly\" {"
-		code << "      date {"
-		code << "        match => [ \"BillingPeriodStartDate\", \"ISO8601\" ]"
-		code << "      }"
-		code << "    }"
-		code << "    mutate {"
-		code << "      replace => [ \"index\", \"aws-billing-%{+YYYY.MM}\"]"
-		code << "      remove_field => [ \"message\" ]"
-		code << "    }"
-		code << "  }"
-		code << ""
-		code << "}"
-		code << ""
-		code << "output {"
-		code << "  elasticsearch {"
-		code << "    embedded => true"
-		code << "    index => \"%{index}\""
-		code << "  }"
-		code << "}"
-		code.join("\n")
-		File.open("/etc/logstash/conf.d/server.conf", "r").each_line do |line|
-		  if line =~ /output/
-		     puts code
-		  end
-		  puts "#{line}"
-		end		
-	end
-	action :run
+tempalte '/etc/logstash/conf.d/aws-billing.conf' do
+  source 'aws-billing-logstash.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  variables({
+    :elasticsearchserver => node['bb_monitor']['logstash']['server']['elasticsearch_server']
+  })
 end
+
 # es template
 template '/tmp/aws-billing-es-template.json' do
   source 'aws-billing-es-template.json.erb'
