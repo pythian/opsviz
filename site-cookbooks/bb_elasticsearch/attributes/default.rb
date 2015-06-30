702fe050cd71b68593e7bfd0a9c9e641043bcb47
Rock Mutchler <rock@drivedev.com>
2015-06-30 17:50:13 -0500

    upgrade es to 1.4.4

--- site-cookbooks/bb_elasticsearch/attributes/default.rb
+++ site-cookbooks/bb_elasticsearch/attributes/default.rb
@@ -1,6 +1,14 @@
-normal[:elasticsearch][:version] = '1.3.4'
-normal[:elasticsearch][:download_url] = 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.4.tar.gz'
-normal[:elasticsearch][:checksum] = '18af629c388b442bc8daa39754ea1f39a606acd7647fe042aed79ef014a7d330'
+#normal[:elasticsearch][:version] = '1.6.0'
+#normal[:elasticsearch][:download_url] = 'https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.6.0.tar.gz'
+#normal[:elasticsearch][:checksum] = 'cb8522f5d3daf03ef96ed533d027c0e3d494e34b'
+
+#1.5.0
+# 07987acd48c754b8e7db6829314b56e1928b5e1b
+#https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.5.0.tar.gz
+
+normal[:elasticsearch][:version] = '1.4.4'
+normal[:elasticsearch][:download_url] = 'https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.4.4.tar.gz'
+normal[:elasticsearch][:checksum] = '963415a9114ecf0b7dd1ae43a316e339534b8f31'
 
 default[:elasticsearch][:http_auth] = false
 #default[:elasticsearch][:http_auth_plugin] = 'https://github.com/Asquera/elasticsearch-http-basic/releases/download/1.3.2/elasticsearch-http-basic-1.3.2.jar'
