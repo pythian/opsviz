32671b52f8d2b98c331c0dea861cfb21bba6f5a1
Rock Mutchler <rock@drivedev.com>
2015-07-01 17:44:57 -0500

    kibana fixes

--- site-cookbooks/bb_monitor/recipes/kibana.rb
+++ site-cookbooks/bb_monitor/recipes/kibana.rb
@@ -5,3 +5,5 @@ Wrapper around upstream kibana recipe to install kibana
 =end
 
 include_recipe "simple-kibana"
+# !ruby/array:Chef::Node::ImmutableArray
+# remove this line from the /opt/kibana/config/kibana.yml
