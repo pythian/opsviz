746306913cabd9227a833738ebe5fbe912ce7032
Alex Lovell-Troy <alex@lovelltroy.org>
2015-07-01 09:17:43 -0700

    upgrading the defaults for elasticsearch

--- site-cookbooks/bb_elasticsearch/attributes/default.rb
+++ site-cookbooks/bb_elasticsearch/attributes/default.rb
@@ -1,8 +1,24 @@
-normal[:elasticsearch][:version] = '1.4.4'
-normal[:elasticsearch][:download_url] = 'https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.4.4.tar.gz'
-# Note that this is the sha256 and not the sha1 hash.  geneteate it with something like:
-# shasum -a 256 ~/Downloads/elasticsearch-1.4.4.tar.gz
-normal[:elasticsearch][:checksum] = 'a3158d474e68520664debaea304be22327fc7ee1f410e0bfd940747b413e8586'
+# Default to 1.6.0 and allow for overrides in the custom_json
+normal[:elasticsearch][:version] = '1.6.0'
+
+# 1.6.0
+if node[:elasticsearch][:version] == '1.6.0'
+    normal[:elasticsearch][:download_url] = 'https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.6.0.tar.gz'
+    normal[:elasticsearch][:checksum] = 'dc336c83394b2f2f72f362e0f959a4cfdec2109aa3de15668401afeab0b02d2e'
+    normal[:elasticsearch][:plugins]["elasticsearch/elasticsearch-cloud-aws"]["version"] = "2.6.0" 
+end
+# 1.5.2
+if node[:elasticsearch][:version] == '1.5.2'
+    normal[:elasticsearch][:download_url] = 'https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.5.2.tar.gz'
+    normal[:elasticsearch][:checksum] = 'efae7897367ac3fa8057d02fad31c72e215b6edef599b63e373d3ce0c1049a14'
+    normal[:elasticsearch][:plugins]["elasticsearch/elasticsearch-cloud-aws"]["version"] = "2.5.1" 
+end
+# 1.4.4
+if node[:elasticsearch][:version] == '1.4.4'
+    normal[:elasticsearch][:download_url] = 'https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.4.4.tar.gz'
+    normal[:elasticsearch][:checksum] = 'a3158d474e68520664debaea304be22327fc7ee1f410e0bfd940747b413e8586'
+    normal[:elasticsearch][:plugins]["elasticsearch/elasticsearch-cloud-aws"]["version"] = "2.4.2" 
+end
 
 default[:elasticsearch][:http_auth] = false
 default[:elasticsearch][:http_auth_plugin] = 'https://github.com/Asquera/elasticsearch-http-basic/releases/download/v1.4.0/elasticsearch-http-basic-1.4.0.jar'
