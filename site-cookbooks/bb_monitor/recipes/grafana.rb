bc6d7d0f7b5e761c6efa8b2428efedba265a020e
Alex Lovell-Troy <alex@lovelltroy.org>
2015-07-02 16:42:52 -0700

    Better dashboarding

--- site-cookbooks/bb_monitor/recipes/grafana.rb
+++ site-cookbooks/bb_monitor/recipes/grafana.rb
@@ -14,9 +14,14 @@ grafana_datasource 'graphite-cluster' do
   )
 end
 
-template "#{node['grafana']['install_dir']}/app/dashboards/default.json" do
-  source 'system_stats.json.erb'
-  owner 'root'
-  group 'root'
-  mode '0664'
+grafana_dashboard 'POVs-self-monitoring:' do
+  source 'self-monitoring'
+  overwrite false
 end
+
+#template "#{node['grafana']['install_dir']}/app/dashboards/default.json" do
+  #source 'system_stats.json.erb'
+  #owner 'root'
+  #group 'root'
+  #mode '0664'
+#end
