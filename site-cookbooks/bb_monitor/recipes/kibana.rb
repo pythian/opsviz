7c75f2a6de5d9fa71f56803bbcfa66aec0658773
Pierig Le Saux <lesaux@pythian.com>
2015-07-20 13:07:13 -0400

    trying simple-kibana cookbook

--- site-cookbooks/bb_monitor/recipes/kibana.rb
+++ site-cookbooks/bb_monitor/recipes/kibana.rb
@@ -4,4 +4,10 @@ Wrapper around upstream kibana recipe to install kibana
 #>
 =end
 
-#include_recipe "kibana_lwrp"
+#node.force_override[:elasticsearch][:custom_config]["http.enabled"] = true
+#node.force_override[:elasticsearch][:custom_config]["node.data"] = false
+#node.force_override[:elasticsearch][:custom_config]["node.master"] = true
+
+#include_recipe 'bb_elasticsearch::default'
+
+include_recipe "simple-kibana"
