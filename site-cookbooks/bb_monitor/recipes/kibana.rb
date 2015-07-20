e01c936b60d5af8bf7aaacc5f8cd6b59a7f48f94
Pierig Le Saux <lesaux@pythian.com>
2015-07-20 13:10:18 -0400

    adding local ES

--- site-cookbooks/bb_monitor/recipes/kibana.rb
+++ site-cookbooks/bb_monitor/recipes/kibana.rb
@@ -4,10 +4,10 @@ Wrapper around upstream kibana recipe to install kibana
 #>
 =end
 
-#node.force_override[:elasticsearch][:custom_config]["http.enabled"] = true
-#node.force_override[:elasticsearch][:custom_config]["node.data"] = false
-#node.force_override[:elasticsearch][:custom_config]["node.master"] = true
+node.force_override[:elasticsearch][:custom_config]["http.enabled"] = true
+node.force_override[:elasticsearch][:custom_config]["node.data"] = false
+node.force_override[:elasticsearch][:custom_config]["node.master"] = true
 
-#include_recipe 'bb_elasticsearch::default'
+include_recipe 'bb_elasticsearch::default'
 
 include_recipe "simple-kibana"
