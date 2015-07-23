77f72b40b2583bfa4fa433f6421ea9288f5d56cc
Pierig Le Saux <lesaux@pythian.com>
2015-07-23 12:54:43 -0400

    sensu log levels

--- site-cookbooks/bb_monitor/recipes/sensu_stack_checks.rb
+++ site-cookbooks/bb_monitor/recipes/sensu_stack_checks.rb
@@ -49,7 +49,7 @@ end
 
 sensu_check "check-elasticsearch-diskspace" do
   command "check-data.rb -s #{node[:graphite][:host]}:8081 -t 'averageSeries(stats.#{node[:opsworks][:stack][:name].downcase}.elasticsearch.*.diskspace.xvdi.capacity)' -a 120 -w 70 -c 80"
-  handlers ["remediator","debug"]
+  handlers ["remediator"]
   subscribers ["dashboard1"]
   interval 30
   additional(:remediation => { :elasticsearch_scale_up => { :occurrences => [1], :severities => [2] }} )
@@ -57,14 +57,14 @@ end
 
 sensu_check "elasticsearch_scale_up" do
   command "scaleOpsworksLayer.rb -s #{node[:opsworks][:stack][:name]} -r #{node[:aws_region]} -l ElasticSearch -i 2 -m 5 -t #{node[:instance_type][:elasticsearch]} -z #{node[:network][:private_subnet0_id]},#{node[:network][:private_subnet1_id]},#{node[:network][:private_subnet2_id]}"
-  handlers [ ]
+  handlers ["debug"]
   subscribers [ ]
   publish false
 end
 
 sensu_check "check-logstash-loadavg" do
   command "check-data.rb -s #{node[:graphite][:host]}:8081 -t 'averageSeries(stats.#{node[:opsworks][:stack][:name].downcase}.logstash.*.load.load_avg.one)' -a 120 -w 2 -c 4"
-  handlers ["remediator","debug"]
+  handlers ["remediator"]
   subscribers ["dashboard1"]
   interval 30
   additional(:remediation => { :logstash_scale_up => { :occurrences => [10], :severities => [1] }} )
@@ -72,14 +72,14 @@ end
 
 sensu_check "logstash_scale_up" do
   command "scaleOpsworksLayer.rb -s #{node[:opsworks][:stack][:name]} -r #{node[:aws_region]} -l Logstash -i 1 -m 5 -t #{node[:instance_type][:logstash]} -z #{node[:network][:private_subnet0_id]},#{node[:network][:private_subnet1_id]},#{node[:network][:private_subnet2_id]}"
-  handlers [ ]
+  handlers ["debug" ]
   subscribers [ ]
   publish false
 end
 
 sensu_check "check-dashboard-loadavg" do
   command "check-data.rb -s #{node[:graphite][:host]}:8081 -t 'averageSeries(stats.#{node[:opsworks][:stack][:name].downcase}.dashboard.*.load.load_avg.one)' -a 120 -w 2 -c 4"
-  handlers ["remediator","debug"]
+  handlers ["remediator"]
   subscribers ["dashboard1"]
   interval 30
   additional(:remediation => { :dashboard_scale_up => { :occurrences => [10], :severities => [1] }} )
@@ -87,7 +87,7 @@ end
 
 sensu_check "dashboard_scale_up" do
   command "scaleOpsworksLayer.rb -s #{node[:opsworks][:stack][:name]} -r #{node[:aws_region]} -l Dashboard -i 1 -m 3 -t #{node[:instance_type][:dashboard]} -z #{node[:network][:private_subnet0_id]},#{node[:network][:private_subnet1_id]},#{node[:network][:private_subnet2_id]}"
-  handlers [ ]
+  handlers ["debug"]
   subscribers [ ]
   publish false
 end
