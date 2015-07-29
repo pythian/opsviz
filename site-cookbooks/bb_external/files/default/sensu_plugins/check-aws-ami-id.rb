d0402c99f889bf9a1a4e2420758b0d4b7fbc9908
Rock Mutchler <phpslave@gmail.com>
2015-07-29 15:58:31 -0500

    sensu check fix

--- site-cookbooks/bb_external/files/default/sensu_plugins/check-aws-ami-id.rb
+++ site-cookbooks/bb_external/files/default/sensu_plugins/check-aws-ami-id.rb
@@ -6,10 +6,10 @@ if not File.exist?("/etc/aws/ami.txt")
 	puts 'WARNING - AMI ID File Not Found'
 	exit 1
 elsif File.zero?("/etc/aws/ami.txt")
-	puts 'WARNING - AMI ID File Not Found'
+	puts 'WARNING - AMI ID File Empty'
   	exit 1
 else
 	data = File.read("/etc/aws/ami.txt")
-	puts 'OK - #{data}'
+	puts "OK - #{data}"
 	exit 0
 end
